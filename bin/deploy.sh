e8e1c21849402de0c1f7ef1f7e7e6d3b6056916d
Jackson Sabol <JacksonSabol@users.noreply.github.com>
2020-03-18 18:36:56 -0700

    update to create react build locally and copy into docker image

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 if git diff-index --quiet HEAD --; then # Check to make sure there are no uncommited changes
     set -o errexit; # Exit on error
-echo Step 1/3: Archiving previous production image;
+echo Step 1/4: Creating optimized React build;
+    npm run build; # Build the React application.
+echo Step 2/4: Archiving previous production image;
     now=$(date +"%m_%d_%Y") # Tag and archive old docker latest. Push to dockerhub.
     docker tag jacksonsabol/portfolio:latest jacksonsabol/portfolio:$now;
     docker push jacksonsabol/portfolio:$now;
-echo Step 2/3: Creating new production image;
+echo Step 3/4: Creating new production image;
     mv Dockerfile.prod.off Dockerfile;
     npm run docker:build; # Build a new latest docker image. Push to dockerhub.
     mv Dockerfile Dockerfile.prod.off;
     npm run docker:push;
-echo Step 3/3: Creating elastic beanstalk environment;
-eb $1 Portfolio # Create the elastic beanstalk environment.;
+echo Step 4/4: Creating/Deploying elastic beanstalk environment;
+eb $1 Portfolio; # Create / deploy to the elastic beanstalk environment depending on the value passed in arg $1.
 else
     echo Please commit your changes first.;
 fi
