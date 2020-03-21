4991b8d8d9a17e5b94f81c7cefad04b4e38de67a
Jackson Sabol <JacksonSabol@users.noreply.github.com>
2020-03-21 15:38:19 -0700

    remove redundant blurring div

--- client/src/Components/PortfolioItem/index.js
+++ client/src/Components/PortfolioItem/index.js
@@ -5,7 +5,6 @@ export function PortfolioItem(props) {
     return (
         <div className="single-portfolio"  style={{ backgroundImage: `url("${props.bgImg}")` }}>
             <button onClick={() => props.handleSelect(props.item)} className="portfolio-info">
-                <div className="pfbg"  style={{ backgroundImage: `url("${props.bgImg}")` }}></div>
                 <h5>{props.projectName}</h5>
                 <p>{props.projectCategory}</p>
             </button>
