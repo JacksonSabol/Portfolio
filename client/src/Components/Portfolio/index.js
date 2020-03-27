bc0b8d5bbf2d3bb59548b4978df5862ee5cbe566
Jackson Sabol <JacksonSabol@users.noreply.github.com>
2020-03-27 15:13:13 -0700

    add project links

--- client/src/Components/Portfolio/index.js
+++ client/src/Components/Portfolio/index.js
@@ -43,7 +43,9 @@ const portfolioItems = [
         thumb: portfolioThumbTwo,
         projectName: "Womps and Chomps",
         projectCategory: "Social Media Application",
-        projectDescription: "This project is an in-progress application for discovering, saving, and sharing electronic music events in the San Francisco Bay area. The main focus of the project is to securely manage login sessions, simulate a relational database, and implement React hooks using the MERN stack. The project follows OWASP session management guidelines to prevent various types of security vulnerabilities, including cross-site scripting (XSS) attacks, cross-site request forgery (CSRF) attacks, brute-force attacks, and NoSQL injection attacks."
+        projectDescription: "This project is an in-progress application for discovering, saving, and sharing electronic music events in the San Francisco Bay area. The main focus of the project is to securely manage login sessions, simulate a relational database, and implement React hooks using the MERN stack. The project follows OWASP session management guidelines to prevent various types of security vulnerabilities, including cross-site scripting (XSS) attacks, cross-site request forgery (CSRF) attacks, brute-force attacks, and NoSQL injection attacks.",
+        githubLink: "https://github.com/JacksonSabol/Womps-And-Chomps",
+        deployedLink: "https://womp-and-chomp.herokuapp.com/"
     },
     {
         key: 2,
@@ -59,7 +61,9 @@ const portfolioItems = [
         thumb: portfolioThumbFour,
         projectName: "Tech News Scraper",
         projectCategory: "Forum Web Application",
-        projectDescription: "A web application that scrapes a popular news website for relevant articles, allowing users to view the articles, comment on the articles, and save them to a profile page if they like them."
+        projectDescription: "A web application that scrapes a popular news website for relevant articles, allowing users to view the articles, comment on the articles, and save them to a profile page if they like them.",
+        githubLink: "https://github.com/JacksonSabol/Scraper-App",
+        deployedLink: "https://news-scraper-js.herokuapp.com/"
     },
     {
         key: 4,
@@ -67,7 +71,9 @@ const portfolioItems = [
         thumb: portfolioThumbFive,
         projectName: "React Concentration Game",
         projectCategory: "Game",
-        projectDescription: "This project is a game built with React.js to help visually demonstrate the mental process of keeping track of React components. It's similar to a standard card game of Concentration, except the cards are face-up React Components that shuffle!"
+        projectDescription: "This project is a game built with React.js to help visually demonstrate the mental process of keeping track of React components. It's similar to a standard card game of Concentration, except the cards are face-up React Components that shuffle!",
+        githubLink: "https://github.com/JacksonSabol/React-Concentration-Game",
+        deployedLink: "https://jacksonsabol.github.io/React-Concentration-Game/"
     },
     {
         key: 5,
@@ -75,7 +81,9 @@ const portfolioItems = [
         thumb: portfolioThumbSix,
         projectName: "Tail Me",
         projectCategory: "SaaS Web Application",
-        projectDescription: "©TailME is an application for professional dog walkers who are dedicated to providing a premium experience to their clients. TailME is designed to minimize the challenges of running a small, dog-walking business by providing tools to intake chosen users, interact with them more meaningfully, and manage accounts and scheduling with greater efficiency."
+        projectDescription: "©TailME is an application for professional dog walkers who are dedicated to providing a premium experience to their clients. TailME is designed to minimize the challenges of running a small, dog-walking business by providing tools to intake chosen users, interact with them more meaningfully, and manage accounts and scheduling with greater efficiency.",
+        githubLink: "https://github.com/JacksonSabol/Tail-Me-App",
+        deployedLink: "https://tail-me.herokuapp.com/"
     },
     {
         key: 6,
@@ -83,7 +91,9 @@ const portfolioItems = [
         thumb: portfolioThumbSeven,
         projectName: "Sparrow Tasker",
         projectCategory: "SaaS Web Application",
-        projectDescription: "Sparrow is a task management system that allows a user to create a list of their 'To Dos'. Sparrow then allows a user to choose to either complete their own task or post it to the global claimboard which allows another person (a Sparrow) to claim the task and complete it on the user's behalf. This allows someone to, in a sense, buy time for themselves to complete other tasks or to spend time with loved ones."
+        projectDescription: "Sparrow is a task management system that allows a user to create a list of their 'To Dos'. Sparrow then allows a user to choose to either complete their own task or post it to the global claimboard which allows another person (a Sparrow) to claim the task and complete it on the user's behalf. This allows someone to, in a sense, buy time for themselves to complete other tasks or to spend time with loved ones.",
+        githubLink: "https://github.com/JacksonSabol/Sparrow-Tasker",
+        deployedLink: "https://sparrow-tasker.herokuapp.com/"
     },
     {
         key: 7,
@@ -91,7 +101,9 @@ const portfolioItems = [
         thumb: portfolioThumbEight,
         projectName: "Google Book Search",
         projectCategory: "Forum Web Application",
-        projectDescription: "This collaborative project is a web application that provides an aesthetically-pleasing User Interface to search for books and add them to a community list. This community list provides an opportunity to be exposed to books you may not have thought to read before, as well as allow you to expose others to your interests!"
+        projectDescription: "This collaborative project is a web application that provides an aesthetically-pleasing User Interface to search for books and add them to a community list. This community list provides an opportunity to be exposed to books you may not have thought to read before, as well as allow you to expose others to your interests!",
+        githubLink: "https://github.com/JacksonSabol/Google-Books-Search",
+        deployedLink: "https://google-books-search-react.herokuapp.com/"
     },
     {
         key: 8,
@@ -99,7 +111,9 @@ const portfolioItems = [
         thumb: portfolioThumbNine,
         projectName: "Eat the Burger",
         projectCategory: "Interactive Web Application",
-        projectDescription: "A burger making and eating application based (loosely) on Amy's© in Petaluma, CA"
+        projectDescription: "A burger making and eating application based (loosely) on Amy's© in Petaluma, CA",
+        githubLink: "https://github.com/JacksonSabol/Eat-The-Burger",
+        deployedLink: "https://eat-the-burger-express.herokuapp.com/"
     },
     {
         key: 9,
@@ -107,7 +121,8 @@ const portfolioItems = [
         thumb: portfolioThumbTen,
         projectName: "Whisker's Whimsies",
         projectCategory: "Command Line Interface",
-        projectDescription: "Whisker's Whimsies is an Amazon-like, Command Line Interface (CLI) storefront for cat products using a SQL database that has three different views: customer view, manager view, and supervisor view. Each view adds progressively higher levels of editability to the inventory of the store. This application is a back-end only application, run through the command line."
+        projectDescription: "Whisker's Whimsies is an Amazon-like, Command Line Interface (CLI) storefront for cat products using a SQL database that has three different views: customer view, manager view, and supervisor view. Each view adds progressively higher levels of editability to the inventory of the store. This application is a back-end only application, run through the command line.",
+        githubLink: "https://github.com/JacksonSabol/Whiskers-Whimsies"
     },
     {
         key: 10,
@@ -115,7 +130,9 @@ const portfolioItems = [
         thumb: portfolioThumbEleven,
         projectName: "Caltrain Trip Planner",
         projectCategory: "Interactive Web Application",
-        projectDescription: "An implementation of Google Firebase to track a local, Bay Area train. Fill out the four required fields containing the train's information: Train Name; Destination; First Train Time; and Frequency. You will be alerted if the train information in the input fields needs modification before submittal. Click the Submit button to add a train line once all the fields are complete. Sit back, relax, and wait for your train to 'arrive'!"
+        projectDescription: "An implementation of Google Firebase to track a local, Bay Area train. Fill out the four required fields containing the train's information: Train Name; Destination; First Train Time; and Frequency. You will be alerted if the train information in the input fields needs modification before submittal. Click the Submit button to add a train line once all the fields are complete. Sit back, relax, and wait for your train to 'arrive'!",
+        githubLink: "https://github.com/JacksonSabol/Train-Scheduler",
+        deployedLink: "https://jacksonsabol.github.io/Train-Scheduler/"
     },
     {
         key: 11,
@@ -123,7 +140,8 @@ const portfolioItems = [
         thumb: portfolioThumbTwelve,
         projectName: "LIRI: Language Interpretation and Recognition Interface",
         projectCategory: "Command Line Interface",
-        projectDescription: "An implementation of Node.js to mimic Apple's Siri. This application is a back-end only application, run through the command line."
+        projectDescription: "An implementation of Node.js to mimic Apple's Siri. This application is a back-end only application, run through the command line.",
+        githubLink: "https://github.com/JacksonSabol/LIRI-Node-App"
     }
 ];
 
