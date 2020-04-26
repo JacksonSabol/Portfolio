f523e0f38ca771b3de3caae15b0ed3fc42262234
Jackson Sabol <JacksonSabol@users.noreply.github.com>
2020-04-25 17:23:30 -0700

    test express validator and mail route

--- mail-routes.js
+++ mail-routes.js
@@ -1,44 +1,63 @@
+// Import dependencies
+require('dotenv').config();
+const nodemailer = require('nodemailer');
+const { check, validationResult } = require('express-validator');
+const validations = [
+    check('name')
+        .trim()
+        .isLength({ min: 3 })
+        .escape()
+        .withMessage('A name is required'),
+    check('email')
+        .trim()
+        .isEmail()
+        .normalizeEmail()
+        .withMessage('A valid email address is required'),
+    check('subject')
+        .trim()
+        .isLength({ min: 3 })
+        .escape()
+        .withMessage('A subject is required'),
+    check('message')
+        .trim()
+        .isLength({ min: 5 })
+        .escape()
+        .withMessage('A message is required'),
+];
+
 module.exports = app => {
-    // Import dependencies
-    require('dotenv').config();
-    const nodemailer = require('nodemailer');
-
-    // Set up connection to mailer account
-    const transporter = nodemailer.createTransport({
-        service: 'gmail',
-        auth: {
-            user: `${process.env.EMAIL_ADDRESS}`,
-            pass: `${process.env.EMAIL_PASSWORD}`,
-        },
-    });
-    const emailTo = `${process.env.emailTo}`;
     // Post route to contact me
-    app.post('/mail/contactme', (req, res, next) => {
-        // Define varables to be used in the automated message
-        const reqName = req.body.name;
-        const reqEmail = req.body.email;
-        const subject = req.body.subject;
-        const message = req.body.message;
-
-        // Define message for nodemailer
-        const mailOptions = {
-            from: {
-                name: reqName,
-                address: reqEmail
-            },
-            to: `${emailTo}`,
-            subject: `Portfolio Mail - ${subject}`,
-            text: message
-        };
-        console.log(mailOptions);
-        // Send mail to client
-        transporter.sendMail(mailOptions, function (err, response) {
-            if (err) {
-                console.error('there was an error: ', err);
-            } else {
-                console.log('here is the res: ', response);
-                res.status(200).json('Email sent');
+    app.post('/mail/contactme', validations, async (req, res, next) => {
+        try {
+            const errors = validationResult(req);
+            if (!errors.isEmpty()) {
+                return res.status(422).send(errors.array());
             }
-        });
+            const emailTo = `${process.env.EMAIL_TO}`;
+            const { name, email, subject, message } = req.body;
+            // Set up connection to mailer account
+            const transporter = nodemailer.createTransport({
+                service: 'gmail',
+                auth: {
+                    user: `${process.env.EMAIL_ADDRESS}`,
+                    pass: `${process.env.EMAIL_PASSWORD}`,
+                },
+            });
+            // Define message for nodemailer
+            const mailOptions = {
+                from: 'Portfolio Mail - jacksonsaboldesign@gmail.com',
+                to: `${emailTo}`,
+                subject: 'New Portfolio Mail',
+                text: `New message from: ${name}\r\nReturn email: ${email}\r\nSubject: ${subject}\r\nMessage:\r\n${message}`
+            };
+            console.log(mailOptions);
+            // Send mail to client
+            const emailRes = await transporter.sendMail(mailOptions);
+            console.log(emailRes);
+            return res.status(200).json('Email sent');
+        } catch (err) {
+            console.error(err);
+            return res.status(422).send(err);
+        }
     })
 };
--- server.js
+++ server.js
@@ -1,7 +1,31 @@
 // Dependencies
 const express = require("express");
 require('dotenv').config();
-// const path = require('path');
+const nodemailer = require('nodemailer');
+const { check, validationResult } = require('express-validator');
+const validations = [
+    check('name')
+        .trim()
+        .isLength({ min: 3 })
+        .escape()
+        .withMessage('A name is required'),
+    check('email')
+        .trim()
+        .isEmail()
+        .normalizeEmail()
+        .withMessage('A valid email address is required'),
+    check('subject')
+        .trim()
+        .isLength({ min: 3 })
+        .escape()
+        .withMessage('A subject is required'),
+    check('message')
+        .trim()
+        .isLength({ min: 5 })
+        .escape()
+        .withMessage('A message is required'),
+];
+
 
 // Define port depending on environment
 const PORT = process.env.PORT || 3001;
@@ -19,8 +43,42 @@ if (process.env.NODE_ENV === "production") {
 }
 
 // Import routes
-require("./mail-routes")(app);
-
+// require("./mail-routes")(app);
+// Post route to contact me
+// validations
+// app.post('/mail/contactme', async (req, res, next) => {
+//     try {
+//         // const errors = validationResult(req);
+//         // if (!errors.isEmpty()) {
+//         //     return res.status(422).send(errors.array());
+//         // }
+//         const emailTo = `${process.env.EMAIL_TO}`;
+//         const { name, email, subject, message } = req.body;
+//         // Set up connection to mailer account
+//         const transporter = nodemailer.createTransport({
+//             service: 'gmail',
+//             auth: {
+//                 user: `${process.env.EMAIL_ADDRESS}`,
+//                 pass: `${process.env.EMAIL_PASSWORD}`,
+//             },
+//         });
+//         // Define message for nodemailer
+//         const mailOptions = {
+//             from: 'Portfolio Mail - jacksonsaboldesign@gmail.com',
+//             to: `${emailTo}`,
+//             subject: 'New Portfolio Mail',
+//             text: `New message from: ${name}\r\nReturn email: ${email}\r\nSubject: ${subject}\r\nMessage:\r\n${message}`
+//         };
+//         console.log(mailOptions);
+//         // Send mail to client
+//         const emailRes = await transporter.sendMail(mailOptions);
+//         console.log(emailRes);
+//         return res.status(200).json('Email sent');
+//     } catch (err) {
+//         console.error(err);
+//         return res.status(422).send(err);
+//     }
+// })
 // Controls
 // Send every GET request to the React app
 // app.get("*", (req, res) => {
