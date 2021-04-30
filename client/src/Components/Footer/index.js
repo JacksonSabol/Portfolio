44179c925fd25f93f49957a7084a2f3ecdd7d547
Jackson Sabol <JacksonSabol@users.noreply.github.com>
2021-04-30 14:02:38 -0700

    fix: match social link to icon

--- client/src/Components/Footer/index.js
+++ client/src/Components/Footer/index.js
@@ -10,8 +10,8 @@ const Footer = () => {
             <h2>Let’s Connect!</h2>
             <div className="social">
                 <a href="https://www.linkedin.com/in/jacksonsabol/" target="_blank" rel="noopener noreferrer" className="social-icon"><img src={linkedinIcon} alt="LinkedIn" /></a>
-                <a href="https://hub.docker.com/u/jacksonsabol" target="_blank" rel="noopener noreferrer" className="social-icon"><img src={githubIcon} alt="GitHub" /></a>
-                <a href="https://github.com/JacksonSabol" target="_blank" rel="noopener noreferrer" className="social-icon"><img src={dockerIcon} alt="Docker" /></a>
+                <a href="https://github.com/JacksonSabol" target="_blank" rel="noopener noreferrer" className="social-icon"><img src={githubIcon} alt="GitHub" /></a>
+                <a href="https://hub.docker.com/u/jacksonsabol" target="_blank" rel="noopener noreferrer" className="social-icon"><img src={dockerIcon} alt="Docker" /></a>
             </div>
         </div>
     );
